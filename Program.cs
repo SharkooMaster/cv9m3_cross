9aaab29143f2f42b97a06fd0fd1b689385f0405c
sharku <alirashini2323@outlook.com>
2025-02-20 16:48:49 +0100

    updated ports

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ builder.Services.AddGrpc();
 // Configure Kestrel to allow HTTP/2 without TLS
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(8100, o => o.Protocols = HttpProtocols.Http2);
-    options.ListenAnyIP(8101, o => o.Protocols = HttpProtocols.Http1);
+    options.ListenAnyIP(5000, o => o.Protocols = HttpProtocols.Http2);
+    options.ListenAnyIP(5001, o => o.Protocols = HttpProtocols.Http1);
 });
 
 builder.Services.AddEndpointsApiExplorer();
