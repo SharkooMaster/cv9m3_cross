38c565bf7b24fa357ef178df6ed3a5fba4de1e4d
sharku <alirashini2323@outlook.com>
2025-02-20 23:11:14 +0100

    update

--- src/Services/Grpc/CompressFileService.cs
+++ src/Services/Grpc/CompressFileService.cs
@@ -7,6 +7,7 @@ public class CompressFileService : FileService.FileServiceBase
 {
     public override async Task<FileResponse> ProcessFile(FileRequest request, ServerCallContext context)
     {
+        Console.WriteLine("got a request");
         FileResponse to_return = new FileResponse();
         return to_return;
     }
