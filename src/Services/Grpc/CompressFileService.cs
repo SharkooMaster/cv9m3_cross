c3161c63c1afffcfab12c18f12b6d7e3bd911445
sharku <alirashini2323@outlook.com>
2025-02-20 23:34:31 +0100

    update

--- src/Services/Grpc/CompressFileService.cs
+++ src/Services/Grpc/CompressFileService.cs
@@ -1,14 +1,19 @@
 
 
+using Cross.Services.Cross;
 using CrossService;
+using Google.Protobuf;
 using Grpc.Core;
 
 public class CompressFileService : FileService.FileServiceBase
 {
     public override async Task<FileResponse> ProcessFile(FileRequest request, ServerCallContext context)
     {
-        Console.WriteLine("got a request");
+        Console.WriteLine("REQUEST RECIEVED");
+        Cross.Services.Cross.CrossService crossService = new Cross.Services.Cross.CrossService();
+
         FileResponse to_return = new FileResponse();
+        to_return.FileContent = ByteString.CopyFrom(await crossService.CompressFile(request.FileContent.ToByteArray()));
         return to_return;
     }
 }
