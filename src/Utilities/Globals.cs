a8e9cd7e7c550df504a33ab995744688e0271c8b
Ali Al Rashini <xxalixx.12388@gmail.com>
2025-03-16 19:08:14 +0000

    Update file Globals.cs

--- src/Utilities/Globals.cs
+++ src/Utilities/Globals.cs
@@ -5,6 +5,6 @@ public static class Globals
 {
     public static int chunkSize = 10240;
     public static int k = 16;
-    public static string GatewayLoadbalancer = "http://gateway-loadbalancer.default.svc.cluster.local:80";
+    public static string GatewayLoadbalancer = "http://10.102.202.233:80";
     public static SearchAllServiceClient searchAllServiceClient = new SearchAllServiceClient();
 }
