932134dfe321a7a197a99aaef96c82ea66135e5c
sharku <alirashini2323@outlook.com>
2025-03-19 15:41:18 +0100

    update

--- src/Utilities/Globals.cs
+++ src/Utilities/Globals.cs
@@ -5,6 +5,6 @@ public static class Globals
 {
     public static int chunkSize = 10240;
     public static int k = 16;
-    public static string GatewayLoadbalancer = "http://192.168.50.241:80";
+    public static string GatewayLoadbalancer = "http://192.168.50.241";
     public static SearchAllServiceClient searchAllServiceClient = new SearchAllServiceClient();
 }
