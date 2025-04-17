3d6fb9c2473ba8fb9bfd2fc4afd0331ba23186c0
sharku <alirashini2323@outlook.com>
2025-04-18 00:35:54 +0200

    update

--- obj/Debug/net8.0/cross.AssemblyInfo.cs
+++ obj/Debug/net8.0/cross.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cross")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b18927bfc8af9cdeecf65fbe3c98d217ec67a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502df3a0f678e209cb5c126422a60d737a060b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("cross")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cross")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Utilities/Globals.cs
+++ src/Utilities/Globals.cs
@@ -3,7 +3,7 @@ namespace Cross.Utilities;
 
 public static class Globals
 {
-    public static int chunkSize = 10240;
+    public static int chunkSize = 1024;
     public static int k = 16;
     public static string GatewayLoadbalancer = "http://192.168.50.241";
     public static SearchAllServiceClient searchAllServiceClient = new SearchAllServiceClient();
