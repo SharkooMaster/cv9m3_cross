0fcf0b5a8259c5d30ef6c7bb7641bf9532135872
sharku <alirashini2323@outlook.com>
2026-02-20 10:16:27 +0100

    update

--- obj/Debug/net8.0/cross.AssemblyInfo.cs
+++ obj/Debug/net8.0/cross.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cross")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d778618e852f3d58389ceb25db64f91fda9cc31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69ff7312c6ee4db9eb15dd91d189f3c42ffc38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("cross")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cross")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
